3655e31432dc05b875207705448b7246f1bcc651
Nate Baer <njbbaer@gmail.com>
2018-10-09 00:05:18 -0700

    Create sleep model

--- self_metrics/settings.py
+++ self_metrics/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sleep.apps.SleepConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
