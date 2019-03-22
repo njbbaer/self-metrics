4c051e01cacdb3e7f96f5ddb98978737dd21bf25
Nate Baer <njbbaer@gmail.com>
2019-03-21 17:25:03 -0700

    Restart project

--- sleep/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SleepConfig(AppConfig):
-    name = 'sleep'
--- sleep/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Sleep(models.Model):
-    start = models.DateTimeField()
-    end = models.DateTimeField()
