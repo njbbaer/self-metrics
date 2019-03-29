5d2fb88302c85943aed9493a83e1cc9d9295b89c
Nate Baer <njbbaer@gmail.com>
2019-03-29 00:17:27 -0700

    Add sleep duration validations

--- app/models/sleep_report.rb
+++ app/models/sleep_report.rb
@@ -1,2 +1,17 @@
 class SleepReport < ApplicationRecord
+  validate :valid_duration?
+
+  def length
+    wakeup_at - asleep_at
+  end
+
+  private
+
+  def valid_duration?
+    if length.negative?
+      errors.add(:base, 'Sleep duration cannot be negative')
+    elsif length > 1.day
+      errors.add(:base, 'Sleep duration cannot greater than a day')
+    end
+  end
 end
