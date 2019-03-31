65b9c8d6051fd5e0b716c9605f0cc8f665e137da
Nate Baer <nate.baer@procore.com>
2019-03-31 12:38:38 -0700

    Scope report routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :weight, as: 'weight_reports', controller: 'weight_reports'
-  resources :sleep, as: 'sleep_reports', controller: 'sleep_reports'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/reports' do
+    resources :weight, as: 'weight_reports', controller: 'weight_reports'
+    resources :sleep, as: 'sleep_reports', controller: 'sleep_reports'
+  end
 end
