b2455662fc8b492f7bbbdedbfd1ab7455c2a0574
Nate Baer <njbbaer@gmail.com>
2019-04-06 22:20:12 -0700

    Create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
   scope '/reports' do
     resources :weight, as: 'weight_reports', controller: 'weight_reports'
     resources :sleep, as: 'sleep_reports', controller: 'sleep_reports'
