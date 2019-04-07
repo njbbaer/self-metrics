d8e5839ecd4f70f2158a66ff78e62f930276d444
Nate Baer <njbbaer@gmail.com>
2019-04-07 02:31:52 -0700

    Rename home resource to summary

--- test/controllers/summary_controller_test.rb
+++ test/controllers/summary_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class SummaryControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get '/'
     assert_response :success
