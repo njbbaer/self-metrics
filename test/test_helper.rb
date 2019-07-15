bd5320abb4a27f89e5def0de9e521516de84052f
Nate Baer <njbbaer@gmail.com>
2019-07-14 20:25:12 -0700

    Don't set default password in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,6 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 
   def authenticate
-    Rails.configuration.password = 'password'
     post sessions_path, params: { password: Rails.configuration.password }
   end
 end
