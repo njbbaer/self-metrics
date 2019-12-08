0eb983ec2295380b669b2fb2ead61f4ce4c470d5
Nate Baer <njbbaer@gmail.com>
2019-12-07 22:50:22 -0800

    Sort weight reports just before use

--- app/services/weight_reports_calculator.rb
+++ app/services/weight_reports_calculator.rb
@@ -2,12 +2,12 @@
 
 class WeightReportsCalculator
   def initialize(weight_reports)
-    @weight_reports = weight_reports.ordered_by_recency
+    @weight_reports = weight_reports
   end
 
   def calculate_exp_avg(alpha:, initial_weight:)
     exp_avg = initial_weight
-    @exp_avg_data = @weight_reports.map do |weight_report|
+    @exp_avg_data = @weight_reports.ordered_by_recency.map do |weight_report|
       exp_avg = alpha * weight_report.weight_pounds + (1 - alpha) * exp_avg
       [weight_report.id, exp_avg]
     end.to_h
@@ -18,7 +18,7 @@ class WeightReportsCalculator
   end
 
   def exp_avg_table
-    @weight_reports.map do |weight_report|
+    @weight_reports.ordered_by_recency.map do |weight_report|
       [weight_report.epoch_timestamp, @exp_avg_data[weight_report.id].round(2)]
     end
   end
