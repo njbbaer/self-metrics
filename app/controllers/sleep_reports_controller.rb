e205b47271b4a1fd23379d2abb0e352b2970208e
Nate Baer <njbbaer@gmail.com>
2020-01-20 12:32:23 -0800

    Fix stop sleep button

--- app/controllers/sleep_reports_controller.rb
+++ app/controllers/sleep_reports_controller.rb
@@ -54,7 +54,7 @@ class SleepReportsController < ApplicationController
 
   # PATCH /stop
   def stop
-    sleep_report = SleepReportsPresenter.all.latest
+    sleep_report = SleepReportsCollection.new.latest
     sleep_report.update!(wakeup_at: Time.now)
     duration_string = sleep_report.duration.strftime('%-Hh %-Mm')
     flash[:success] = "Goodmorning! You slept for #{duration_string}"
