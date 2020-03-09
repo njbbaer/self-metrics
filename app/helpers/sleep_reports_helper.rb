9fd408d4a10e3671dc53a23cab5a914b0d3e871e
Nate Baer <njbbaer@gmail.com>
2020-03-09 01:05:53 -0700

    Remove sleep duration color coding

--- app/helpers/sleep_reports_helper.rb
+++ app/helpers/sleep_reports_helper.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 module SleepReportsHelper
-  def sleep_duration_color_style(duration)
-    hours = duration.hours
-    float =
-      if hours > 9 then 1
-      elsif hours < 6 then 0
-      else (hours - 6) / 3.0
-      end
-    normalized_color_style(float)
-  end
-
   def restedness_score_color_style(score)
     float = ([score, 60].max - 60) / 40
-    normalized_color_style(float)
-  end
-
-  def normalized_color_style(float)
     red = float > 0.5 ? (1 - float) * 2 * 255 : 255
     green = float < 0.5 ? float * 2 * 255 : 255
     "rgb(#{red},#{green},0)"
