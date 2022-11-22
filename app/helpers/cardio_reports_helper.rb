a099893f5c3b81a9be31d6871b259f442b30b920
Nate Baer <njbbaer@gmail.com>
2022-11-21 23:57:16 -0800

    Properly display durations over 59 minutes

--- app/helpers/cardio_reports_helper.rb
+++ app/helpers/cardio_reports_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module CardioReportsHelper
+  def strftime_duration(duration)
+    strf_string = duration.hours >= 1 ? '%k:%M:%S' : '%M:%S'
+    duration.strftime(strf_string)
+  end
 end
