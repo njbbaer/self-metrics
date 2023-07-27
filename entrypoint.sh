c7d8c643747c019bacbd7d70cd3c7caa301e41a2
Nate Baer <njbbaer@gmail.com>
2023-07-26 23:40:35 -0700

    Switch to db:prepare

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 # Ensure server PID file does not exist
 rm -f /self-metrics/tmp/pids/server.pid
 
-bundle exec rails db:version || bundle exec rails db:setup
-bundle exec rails db:migrate
+bundle exec rails db:prepare
 
 exec "$@"
