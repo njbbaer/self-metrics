5c8377d10efb1acb75fb6c0371ccf88556bfd141
Nate Baer <njbbaer@gmail.com>
2023-07-28 00:05:30 -0700

    Restore old docker user name and add check to ensure the user and group do not already exist.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,15 @@ set -e
 
 rm -f /self-metrics/tmp/pids/server.pid
 
-groupadd -g $PGID dockeruser
-useradd -m -s /bin/bash -u $PUID -g $PGID dockeruser
-chown -R dockeruser:dockeruser /self-metrics
+if ! getent group appuser >/dev/null; then
+    groupadd -g $PGID appuser
+fi
 
-gosu dockeruser bundle exec rails db:prepare
-exec gosu dockeruser "$@"
+if ! getent passwd appuser >/dev/null; then
+    useradd -m -s /bin/bash -u $PUID -g $PGID appuser
+fi
+
+chown -R appuser:appuser /self-metrics
+
+gosu appuser bundle exec rails db:prepare
+exec gosu appuser "$@"
